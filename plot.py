44310f000c7e287737ca733febb1ad7e6386f0f2
jamiejann <jamie.jann@gmail.com>
2019-03-27 14:51:09 -0700

    changed to use MaxNLocator for x/y axis tickers

--- plot.py
+++ plot.py
@@ -1,6 +1,5 @@
 import csv
 import matplotlib.pyplot as plt
-from matplotlib import ticker
 from matplotlib.ticker import MaxNLocator
 
 time_list = []
@@ -22,55 +21,42 @@ del remaining_list[0]
 del reserved_list[0]
 del disbursed_list[0]
 
-len_normalized_xaxis = len(time_list) / 10
-len_normalized_yaxis = len(time_list) / 5
-
 # initialize 3 by 1 plots, invert y axis for python misprint
 plt.subplot(3, 1, 1)
 plt.plot(time_list, remaining_list)
 plt.title('Remaining Funds')
 plt.gca().invert_yaxis()
 
-# show only 5 y labels
+# y-axis normalization
 ax = plt.gca()
-#ax.yaxis.set_major_locator(ticker.maxNLocator(integer=True))
-ax.set_yticks(ax.get_yticks()[::2])
-#remaining_list.remove("")
-#plt.yticks(max(remaining_list), min(remaining_list))
-
+ylocator = MaxNLocator(prune="both", nbins=5)
+ax.yaxis.set_major_locator(ylocator)
 
 plt.subplot(3, 1, 2)
 plt.plot(time_list, reserved_list)
 plt.title('Reserved Funds')
 
 ax = plt.gca()
-ax.set_yticks(ax.get_yticks()[::2])
-
-"""
-ax = plt.gca()
-for label in ax.get_yaxis().get_ticklabels():
-    label.set_visible(False)
-for label in ax.get_yaxis().get_ticklabels()[::len_normalized_yaxis]:
-    label.set_visible(True)
-"""
+ylocator = MaxNLocator(prune="both", nbins=5)
+ax.yaxis.set_major_locator(ylocator)
 
 plt.subplot(3, 1, 3)
 plt.plot(time_list, disbursed_list)
 plt.title('Disbursed Funds')
 plt.xlabel('Time')
 
+# y-axis normalization
 ax = plt.gca()
+ylocator = MaxNLocator(prune="both", nbins=5)
+ax.yaxis.set_major_locator(ylocator)
 
 
+# x-axis normalization
+xlocator = MaxNLocator(prune = "both", nbins=10)
+ax.xaxis.set_major_locator(xlocator)
 
-# show only 10 x labels
-for label in ax.get_xaxis().get_ticklabels():
-    label.set_visible(False)
-for label in ax.get_xaxis().get_ticklabels()[::len_normalized_xaxis]:
-    label.set_visible(True)
-
+# prettify dates
 plt.gcf().autofmt_xdate()
-
 plt.tight_layout()
 
 plt.show()
