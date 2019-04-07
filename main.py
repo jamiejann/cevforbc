b60032262d2e3b6929d2eb14bddc8e39eca04d81
jamiejann <jamie.jann@gmail.com>
2019-04-07 14:08:43 -0700

    updated with consistent dataset

--- main.py
+++ main.py
@@ -19,76 +19,74 @@ hdr = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
 
-while 1:
 
-    """driver code (headless)"""
-    options = Options()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome('chromedriver.exe', options=options)
-    driver.get(url)
+"""driver code (headless)"""
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome('chromedriver.exe', options=options)
+driver.get(url)
 
-    bs = Soup(driver.page_source, 'html.parser')
+bs = Soup(driver.page_source, 'html.parser')
 
-    # close the chrome driver for less console time
-    driver.quit()
+# close the chrome driver for less console time
+driver.quit()
 
-    funds_container = bs.findAll("cufon", class_="cufon")
+funds_container = bs.findAll("cufon", class_="cufon")
 
-    funds_remaining = int(re.sub(",", "", funds_container[1].get('alt')))
-    funds_reserved = int(re.sub(",", "", funds_container[3].get('alt')))
-    funds_disbursed = int(re.sub(",", "", funds_container[5].get('alt')))
+funds_remaining = int(re.sub(",", "", funds_container[1].get('alt')))
+funds_reserved = int(re.sub(",", "", funds_container[3].get('alt')))
+funds_disbursed = int(re.sub(",", "", funds_container[5].get('alt')))
 
-    # get current time
-    location = pytz.timezone('America/Los_Angeles')
-    current_time = datetime.datetime.now(tz=location)
+# get current time
+location = pytz.timezone('America/Los_Angeles')
+current_time = datetime.datetime.now(tz=location)
 
-    current_time = current_time.replace(second=0, microsecond=0, tzinfo=None)
+current_time = current_time.replace(second=0, microsecond=0, tzinfo=None)
 
-    # check whether there already exists data
-    if os.path.isfile('./data.csv'):
+# check whether there already exists data
+if os.path.isfile('./data.csv'):
 
-        fields = [current_time,
-                  funds_remaining,
-                  funds_reserved,
-                  funds_disbursed]
+    fields = [current_time,
+              funds_remaining,
+              funds_reserved,
+              funds_disbursed]
 
-        try:
-            with open(r'data.csv', 'ab') as f:
-                writer = csv.writer(f)
-                writer.writerow(fields)
-        except:
-            pass
-
-        print([current_time,
-               funds_remaining,
-               funds_reserved,
-               funds_disbursed])
-
-    # if first time creating data.csv
-    else:
-        headers = ['time', 'remaining', 'reserved', 'disbursed']
-
-        try:
-            with open(r'data.csv', 'wb') as f:
-                writer = csv.writer(f)
-                writer.writerow(headers)
-                writer.writerow([current_time,
-                                 funds_remaining,
-                                 funds_reserved,
-                                 funds_disbursed])
-        except:
-            pass
-
-        print([current_time,
-               funds_remaining,
-               funds_reserved,
-               funds_disbursed])
-
-    # check whether file was opened
     try:
-        f
-        f.close()
+        with open(r'data.csv', 'ab') as f:
+            writer = csv.writer(f)
+            writer.writerow(fields)
     except:
         pass
 
-    time.sleep(1800)
+    print([current_time,
+           funds_remaining,
+           funds_reserved,
+           funds_disbursed])
+
+# if first time creating data.csv
+else:
+    headers = ['time', 'remaining', 'reserved', 'disbursed']
+
+    try:
+        with open(r'data.csv', 'wb') as f:
+            writer = csv.writer(f)
+            writer.writerow(headers)
+            writer.writerow([current_time,
+                             funds_remaining,
+                             funds_reserved,
+                             funds_disbursed])
+    except:
+        pass
+
+    print([current_time,
+           funds_remaining,
+           funds_reserved,
+           funds_disbursed])
+
+# check whether file was opened
+try:
+    f
+    f.close()
+except:
+    pass
+
